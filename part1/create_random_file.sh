19b7f17d30234d51ec183cdcab05b15e9478aca2
Chris Lu <chrislu0126@hotmail.com>
2018-02-20 15:37:59 -0500

    sh uploaded

---
+++ part1/create_random_file.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+make
+
+echo test 
+./create_random_file random.txt 10000000 100
+echo test 
+./create_random_file random.txt 10000000 430
+echo test 
+./create_random_file random.txt 10000000 760
+echo test 
+./create_random_file random.txt 10000000 1090
+echo test 
+./create_random_file random.txt 10000000 1420
+echo test 
+./create_random_file random.txt 10000000 1750
+echo test 
+./create_random_file random.txt 10000000 2080
+echo test 
+./create_random_file random.txt 10000000 2410
+echo test 
+./create_random_file random.txt 10000000 2740
+echo test 
+./create_random_file random.txt 10000000 3072
